26f7ac33738c0f3114492eb1c385ead9f4ddb2da
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-04 09:51:30 +0900

    커밋 테스트

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//글자 주황색은 예약어, 키워드
+class Main {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+    }
+}
