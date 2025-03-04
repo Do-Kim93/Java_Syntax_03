c764b6c95437ab3e5d66ce1b5ad37406ace089e7
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-04 10:05:33 +0900

    커밋 테스트2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ class Main {
         System.out.println("안녕하세요");
         System.out.println("안녕하세요");
         System.out.println("안녕하세요");
-        System.out.println("안녕하세요");
+        System.out.println("git 테스트");
+        System.out.println("git 테스트");
     }
 }
