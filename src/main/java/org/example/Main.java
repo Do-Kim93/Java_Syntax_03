e8de4103897b40afcfdf94677102681513df60b9
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-04 10:34:16 +0900

    커밋 테스트3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요");
         System.out.println("안녕하세요");
-        System.out.println("안녕하세요");
         System.out.println("git 테스트");
-        System.out.println("git 테스트");
+        System.out.println("git commit 순서 테스트");
     }
 }
