412ff85d79afca789dcbcfd15b478f1004991735
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-05 11:47:45 +0900

    while문과 증감연산자  기본형태

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,22 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        int i =1;
+        int i =1; //1.번 재료 만들기
         //while 반복문
-        while (i <= 100) {
-            System.out.println(i);
-            i++;
+        while (i <= 10) {//2번 조건넣기
+            System.out.println(i); // 필요한거 넣기
+            i++; // 증감연산자로 무한반복 안되게 하기
         }
 
         System.out.println("i : " + i);
+
+        int j = 10;
+        //while 반복문
+        while (j >= 1) {
+            System.out.println(j);
+            j--;
+        }
+
+        System.out.println("j : " + j);
     }
 }
