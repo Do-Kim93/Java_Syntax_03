ad052146453529494655a926d38faa5fb4e7bf11
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-06 12:32:59 +0900

    코드업 1026문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,21 @@
 package org.example;
 
 import java.util.Scanner;
-
+// 코드업 1026문제
 class Main {
     public static void main(String[] args) {
-        //코드업 1023문제 문자열로 받아서 자르면 간단하지만 문제 요구사항 만족이 안됨
-        //테스트 케이스 있는 실수를 float으로 받으면 제대로 값이 안들어옴
-        //double를 사용해서 풀면 가능함
         Scanner sc = new Scanner(System.in);
-        Double a;
-        a = sc.nextDouble();
-//        System.out.printf("%.5f",a);// 애초에 입력값을 제대로 가져오지 못함
-        String s = Double.toString(a);
-        String[] b = s.split("\\.");
-        System.out.print(b[0]+"\n"+b[1]);
+        int b = sc.nextInt();
+        String a = Integer.toString(b);
+        String[] c = a.split("");
+//        System.out.println(c.length);
+        for(int i=0; i <= c.length-1; i++){
+            System.out.print("["+c[i]);
+            for(int l = c.length-2; l >= i; l--){
+                System.out.print(0);
+            }
+            System.out.print("]");
+            System.out.println();
+        }
     }
 }
