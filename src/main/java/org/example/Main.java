b7379f9b21d14334c71b2fefd13b9ca121f7f522
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-07 16:55:02 +0900

    0307 배열문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a>b?a:b);
-        //신기하게 경고가 떠서 눌러보니 Math.min이라는걸 사용하라고 한다
-        // 써보니 입력받은 두수 중 작은 숫자를 출력해준다
-        // 여기서 알수있는점은 min이 작은숫자니까 max는 큰숫자라를 출력해줄것이다.
-
-
+        boolean[] arr1 = new boolean[] {true, false, true};
+        double[] arr2 = new double[] {3.14, 7.77, 11.11};
+        int[] arr3 = new int[10];
+        System.out.printf("%b,%b,%b\n",arr1[0],arr1[1],arr1[2]);
+        System.out.printf("%.2f,%.2f,%.2f\n",arr2[0],arr2[1],arr2[2]);
+        for(int i=0; i <= arr3.length-1; i++){
+            arr3[i] = i+1;
+            System.out.print(arr3[i]);
+            if(i != 9){
+                System.out.print(", ");
+            }
+        }
     }
 }
 
