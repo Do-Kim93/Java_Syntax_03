95e4e7b4b5cf609ba615c50ae6e33d2fef5640da
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-07 17:26:44 +0900

    배열 이용한 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        boolean[] arr1 = new boolean[] {true, false, true};
-        double[] arr2 = new double[] {3.14, 7.77, 11.11};
-        int[] arr3 = new int[10];
-        System.out.printf("%b,%b,%b\n",arr1[0],arr1[1],arr1[2]);
-        System.out.printf("%.2f,%.2f,%.2f\n",arr2[0],arr2[1],arr2[2]);
-        for(int i=0; i <= arr3.length-1; i++){
+        boolean[] arr1 = new boolean[] {true, false, true};//1번 문제
+        double[] arr2 = new double[] {3.14, 7.77, 11.11};//2번 문제
+        int[] arr3 = new int[10];//3번 문제
+        System.out.printf("%b,%b,%b\n",arr1[0],arr1[1],arr1[2]);//1번 문제
+        System.out.printf("%.2f,%.2f,%.2f\n",arr2[0],arr2[1],arr2[2]);//2번 문제
+        for(int i=0; i <= arr3.length-1; i++){//3번 문제
             arr3[i] = i+1;
             System.out.print(arr3[i]);
             if(i != 9){
