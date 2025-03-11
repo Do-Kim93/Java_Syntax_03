5e4d0655556aeaa8c1dfbdacaf2a9cbc2c7a8a89
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-11 14:05:30 +0900

    코드 해석하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,35 @@
 package org.example;
 
-// Main 클래스 안의 내용을 올바르게 수정해주세요.
+import java.awt.*;
+import java.util.Arrays;
 
-class Main {
-    public static void main(String[] args) {
-//        int 자동차 = new 자동차; 틀린구문 틀린이유 타입도 맞지않고 ()도 없음
-        자동차 붕붕이 = new 자동차();
-//        자동차.달립니다(); 틀린이유 매서드명도 다르고 자동차 class에 있는 메서드는 스태틱이 아니라서 객체 생성없이는 호출 불가능 해결 방벙 2개 있음
-        //1번 해결방법
-        붕붕이.달리다();
-        //2번 해결방법
-        자동차.달립니다();
-    }
-}
+        class Main {
+            public static void main(String[] args) {
+                String msg = greet("Jin"); //문자열 타입의 변수명 msg에 greet 메서드를 할당
+                System.out.println(msg);// msg 출력
 
-class 자동차 {
-    void 달리다() {
-        System.out.println("자동차가 달립니다.");
-    }
-    //스태틱 매서드로 선언하면 객체 생성없이 클래스명으로 접근이 가능해져서 별도의 객체 생성없이 사용가능
-    static void 달립니다() {
-        System.out.println("자동차가 달립니다.1");
-    }
-}
+                Point point = createPoint(5, 10); //Point 타입의 변수명 point에 인자값이 5랑 10을 가지는 createPoint라는 매서드를 할당
+                System.out.println("Point create at (" + point.x + ", " + point.y + ")");// 출력
+
+                int[] evens = firstNEvenNumbers(5);//int타입의 배열 변수명 evens에 인자값 5를 가지는 firstNEvenNumbers라는 메서드를 할당
+                System.out.println("first 5 even numbers : " + Arrays.toString(evens));//출력
+            }
+
+            public static String greet(String name) {//스태틱변수로 린턴값이 존재하고 문자열로 된 매개변수 1개를 가지는 매서드 greet 선언
+                return "Hello, " + name + "!";//리턴값 "Hello, " + name(인자로 전달 받을거임) + "!" 를 반환함
+            }
+
+            public static Point createPoint(int x, int y) {// 스태틱변수로 린턴값이 존재하고 인트형 으로된 매개변수2개를 가지는 매서드 생성
+                return new Point(x, y);// 받은 인자값으로 객체를 만들어서 리턴 여기서 객체로 재생성하는 이유는 return 값이 두개의 값을 할당 받을수 없어서
+            }
+
+            public static int[] firstNEvenNumbers(int n) {//스태틱변수로 린턴값이 존재하고 정수형 매개변수1개를 가지는 매서드 생성
+                int[] evenNumbers = new int[n];// 받은 인자값으만큼 배열생성
+
+                for (int i = 0, num = 2; i < n; i++, num += 2) {//배열 처음부터 마지막까지 2의 배수를 할당
+                    evenNumbers[i] = num;//배열 처음부터 마지막까지 2의 배수를 할당
+                }
+                return evenNumbers;//값이 할당된 배열 전체를 리턴
+            }
+        }
+
