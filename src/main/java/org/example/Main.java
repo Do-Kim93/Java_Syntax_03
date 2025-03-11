b8a39eeefe0282b9b6a7f8bc0b8ac17a1b8da6ed
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-11 10:51:33 +0900

    매개변수가 있는 메서드

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //
 class Main {
     public static void main(String[] args) {
-        계산기.합(10, 20);
+        계산기.합(10, 20);//()안에 있는 데이터를 인자라고 한다.
         계산기.합(20, 50);
         계산기2 a = new 계산기2();
         a.합(1,2);
@@ -13,7 +13,7 @@ class Main {
 }
 
 class 계산기 {
-    static void 합(int a, int b) {//이럼 모양을 매개변수가 있는 메서드라고 한다.()안에 있는 데이터를 인자라고 한다.
+    static void 합(int a, int b) {//이럼 모양을 매개변수가 있는 메서드라고 한다.
         System.out.println(a + b);
     }
 
