64687bb90c08303489cc5e6689964fe8c0f4080a
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-11 13:00:24 +0900

    함수 만들기 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,47 @@
 package org.example;
 
 import java.util.Scanner;
-
-//리턴 값이 있는 매서드
+// 표준 입력장치로 숫자를 입력받아 합을 구하는 함수 만들기
 class Main {
-
-    static int a = 20; //전역변수
-
     public static void main(String[] args) {
-//        System.out.println(a);
-
-        int a = 30; //지역변수
-        System.out.println(계산기.합(10, 20));//이코드를 실행하면 합 매서드의 출력문이 출력되고 리턴 값으로 30을 가져와서 다시 한번 30이 출력돼 총 2번이 출력된다.
-        System.out.println("===================================");
-        계산기.합(20, 20);//이코드를 실행하면 합 매서드의 출력문이 출력되고 리턴 값으로 40을 가져오지만 출력문이 없어 총 한번만 출력된다.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요");
+        int a = sc.nextInt();
+        System.out.println(sum.sum1(a));
+        System.out.println("숫자를 입력하세요");
+        int b = sc.nextInt();
+        System.out.println("숫자를 입력하세요");
+        int c = sc.nextInt();
+        System.out.println(sum.sum2(b, c));
 
     }
 }
 
-class 계산기 {
-    int a = 50;
-    static int 합(int a, int b) {// void가 없으면 어떠한 타입이든 리턴 값이 있어야한다.
-//        int a = 50;
-        System.out.println(a + b);
+//        문제 - 1부터 n까지의 합을 반환하는 함수
+class sum {
 
-        return a+b;
+    static int sum1(int i) {
+        int sum = 0;
+        for (int a = 1; a <= i; a++) {
+            sum += a;
+        }
+        System.out.println("1부터" + i + "까지 더한 합은");
+        return sum;
+    }
+
+    //        문제 - n부터 m까지의 합을 반환하는 함수
+    static int sum2(int b, int i) {
+        int sum = 0;
+        if (b > i) {
+            for (int l = b; i <= l; i++) {
+                sum += i;
+            }
+        } else {
+            for (int l = i; b <= l; b++) {
+                sum += b;
+            }
+        }
+        System.out.println("1부터" + i + "까지 더한 합은");
+        return sum;
     }
 }
