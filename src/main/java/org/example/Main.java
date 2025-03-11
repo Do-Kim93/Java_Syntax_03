74d577ab949b6896c67205557e1d1cc98497dd58
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-11 11:46:00 +0900

    리턴값이 있는 메서드

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,28 @@ package org.example;
 
 import java.util.Scanner;
 
-//
+//리턴 값이 있는 매서드
 class Main {
+
+    static int a = 20; //전역변수
+
     public static void main(String[] args) {
-        계산기.합(10, 20);//()안에 있는 데이터를 인자라고 한다.
-        계산기.합(20, 50);
-        계산기2 a = new 계산기2();
-        a.합(1,2);
+//        System.out.println(a);
+
+        int a = 30; //지역변수
+        System.out.println(계산기.합(10, 20));//이코드를 실행하면 합 매서드의 출력문이 출력되고 리턴 값으로 30을 가져와서 다시 한번 30이 출력돼 총 2번이 출력된다.
+        System.out.println("===================================");
+        계산기.합(20, 20);//이코드를 실행하면 합 매서드의 출력문이 출력되고 리턴 값으로 40을 가져오지만 출력문이 없어 총 한번만 출력된다.
+
     }
 }
 
 class 계산기 {
-    static void 합(int a, int b) {//이럼 모양을 매개변수가 있는 메서드라고 한다.
+    int a = 50;
+    static int 합(int a, int b) {// void가 없으면 어떠한 타입이든 리턴 값이 있어야한다.
+//        int a = 50;
         System.out.println(a + b);
-    }
 
-}
-class 계산기2 {
-    void 합(int a, int b) {
-        System.out.println(a + b);
+        return a+b;
     }
-
 }
