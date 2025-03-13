4f6eaf33703ff34cc3a9d52d100275e4d6fffe34
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-13 16:16:35 +0900

    생성자1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,54 +1,37 @@
 package org.example;
 
-//올바른 리턴타입으로 메서드를 만들어주세요
-//많이 중요!
 public class Main {
     public static void main(String[] args) {
-        int i = 로봇.get정수();
-        boolean b = 로봇.get논리();
-        사람 a사람 = 로봇.get사람();
-        사람 a사람2 = 로봇.get사람2();
-        사람 a사람3 = 로봇.get사람3();
-        사람 a사람4 = 로봇.get사람4();
-        사람 a사람5 = 로봇.get사람5(123, false);
-        로봇.get사람6(123, false);
+        사람 사람1 = new 사람("김철수");
+//        사람1.이름 = "김철수";
+//        사람1.나이 = 22;
+//        사람1.나이를_22로_설정();
+        System.out.println("==사람1 정보==");
+        System.out.printf("이름 : %s\n", 사람1.이름);
+        System.out.printf("나이 : %d\n", 사람1.나이);
+        사람 사람2 = new 사람();
+        사람2.이름 = "이영희";
+//        사람2.나이 = 22;
+//        사람2.나이를_22로_설정();
+        System.out.println("==사람1 정보==");
+        System.out.printf("이름 : %s\n", 사람2.이름);
+        System.out.printf("나이 : %d\n", 사람2.나이);
+        사람 사람3 = new 사람();
     }
 }
 class 사람 {
-    int 학번;
-    boolean 출결;
-}
-class 로봇 {
-    static int get정수() { //new로봇 안했으니까 스태틱임
-        return 0;
-    }
-    static boolean get논리() {
-        return true;
-    }
-    static 사람 get사람() {
-        return null;
-    }
-    static 사람 get사람2() {
-        return new 사람();//리턴값으로 객체를 넘김
-    }
-    static 사람 get사람3() {
-        사람 a사람 = null;
-        return a사람;
-    }
+    String 이름;
+    int 나이;
 
-    static 사람 get사람4() {
-        사람 a사람 = new 사람();
-        return a사람;
+//    void 나이를_22로_설정(){
+//        this.나이 = 22;
+//    }
+    사람() {//사람이라고 쓴이유는 생성자이름은 클래스명이랑 동일해야함
+        System.out.println("매개변수 0");
+        this.나이 = 22;
     }
-
-    static 사람 get사람5(int i, boolean b) {
-        사람 a사람 = new 사람();
-        a사람.학번 = i;
-        a사람.출결 =b;
-        return a사람;
-    }
-
-    static void get사람6(int i, boolean b) {
+    사람(String 이름) {//사람이라고 쓴이유는 생성자이름은 클래스명이랑 동일해야함
+        System.out.println("매개변수 1");
+        this.이름 = 이름;
     }
 }
-
