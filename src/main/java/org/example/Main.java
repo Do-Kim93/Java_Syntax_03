6e9542e90a5f40ae255b82bbd517785626d21809
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-13 16:28:07 +0900

    생성자 기본

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,36 +2,43 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        사람 사람1 = new 사람("김철수");
+        사람 사람1 = new 김철수("김철수2");
 //        사람1.이름 = "김철수";
-//        사람1.나이 = 22;
-//        사람1.나이를_22로_설정();
         System.out.println("==사람1 정보==");
         System.out.printf("이름 : %s\n", 사람1.이름);
         System.out.printf("나이 : %d\n", 사람1.나이);
         사람 사람2 = new 사람();
         사람2.이름 = "이영희";
-//        사람2.나이 = 22;
-//        사람2.나이를_22로_설정();
         System.out.println("==사람1 정보==");
         System.out.printf("이름 : %s\n", 사람2.이름);
         System.out.printf("나이 : %d\n", 사람2.나이);
-        사람 사람3 = new 사람();
+
+        전사 전사 = new 전사();
     }
 }
+class 전사{
+
+}
+
 class 사람 {
     String 이름;
     int 나이;
 
-//    void 나이를_22로_설정(){
-//        this.나이 = 22;
-//    }
     사람() {//사람이라고 쓴이유는 생성자이름은 클래스명이랑 동일해야함
-        System.out.println("매개변수 0");
+        System.out.println("사람 생성자 실행됨");
         this.나이 = 22;
     }
-    사람(String 이름) {//사람이라고 쓴이유는 생성자이름은 클래스명이랑 동일해야함
-        System.out.println("매개변수 1");
+}
+
+class 김철수 extends 사람 {//상속받은 클래스의 생성자를 생성하면 부모클래스의 생성자도 자동으로 생성된다.
+
+    김철수() {
+        System.out.println("김철수 생성자 실행됨");
+        this.이름 = "김철수";
+    }
+
+    김철수(String 이름) {
+        System.out.println("김철수2 생성자 실행됨");
         this.이름 = 이름;
     }
 }
