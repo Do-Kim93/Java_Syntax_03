20a1cac54db7c68c40fca1095a4422c17f916832
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-13 10:31:48 +0900

    추상매서드

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,183 @@
 package org.example;
-
-// 문제 : 매개변수를 사용해서 전사가 매번 다르게 공격하도록 해주세요.
+// 상황 : DVD방에 3개의 서로다른 tv가 있다. 영업을 하다보니 각 tv의 특별한 기능은 거의 사용될 일이 없고 기본적인 켜기/끄기 만 사용된다. 현재 리모콘이 3개나 있는습니다.
+// 문제 : 알바를 편하게 하기 위해서 표준Tv 리모콘을 구현해주세요.
 
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
 
-        a전사.공격("브라이언","칼");
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        a전사.재공격();
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        a샤오미Tv.켜기();
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기();
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기();
+        // 출력 => 샤오미Tv vr켜기!
 
-        a전사.공격("필립","창");
-        // 필립이(가) 창(으)로 공격합니다.
+        a삼성Tv.켜기();
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기();
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기();
+        // 출력 => 삼성Tv ar켜기!
 
-        a전사.공격("마크","지팡이");
-        // 마크(가) 지팡이(으)로 공격합니다.
+        aLGTv.켜기();
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기();
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환();
+        // 출력 => LGTv 게임모드전환!
 
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
+        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
 
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
+        // 표준Tv 리모콘을 만든다.
+        표준Tv a표준Tv;
+
+        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
+        a표준Tv = a샤오미Tv;
+        a표준Tv.켜기();
+        // 출력 : 샤오기TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 샤오기TV 꺼집니다.
+
+        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
+        a표준Tv = a삼성Tv;
+        a표준Tv.켜기();
+        // 출력 : 삼성TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 삼성TV 꺼집니다.
+
+        // a표준Tv 변수에 LGTv 객체를 연결한다.
+        a표준Tv = aLGTv;
+        a표준Tv.켜기();
+        // 출력 : LGTV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : LGTV 꺼집니다.
+
+        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
+        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
+        LGTv aLGTv2 = (LGTv)a표준Tv;
+        aLGTv2.게임모드전환();
     }
 }
-class 전사{
 
-    String lastName;//전역 변수 만들어서 함수 호출이 끝나도 살아있음
-    String lastWeapon;
-
-     void 공격(String name, String weapon){
-         System.out.println(name+"(가)"+weapon+"(으)로 공격합니다.");
-         lastName = name;
-         lastWeapon = weapon;
+class 샤오미Tv extends 표준Tv{
+    void 켜기() {
+        System.out.println("샤오미Tv 켜집니다.");
     }
-     void 재공격(){
-         System.out.println(this.lastName+"(가)"+this.lastWeapon+"(으)로 공격합니다.");
+
+    void 끄기() {
+        System.out.println("샤오미Tv 꺼집니다.");
+    }
+
+    void vr켜기() {
+        System.out.println("샤오미Tv vr켜기!");
     }
 }
+
+class 삼성Tv extends 표준Tv{
+    void 켜기() {
+        System.out.println("삼성Tv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("삼성Tv 꺼집니다.");
+    }
+
+    void ar켜기() {
+        System.out.println("삼성Tv ar켜기!");
+    }
+}
+
+class LGTv extends 표준Tv{
+    void 켜기() {
+        System.out.println("LGTv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("LGTv 꺼집니다.");
+    }
+
+    void 게임모드전환() {
+        System.out.println("LGTv 게임모드전환!");
+    }
+}
+abstract class 표준Tv {//LGTv 때문에 인터페이스로 전환 불가
+    abstract void 켜기();
+    abstract void 끄기();
+    
+
+}
+
+
+
+
+
+
+// 문제 : 아래 코드가 실행되도록 해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//        샤오미Tv a샤오미Tv = new 샤오미Tv();
+//        삼성Tv a삼성Tv = new 삼성Tv();
+//        LGTv aLGTv = new LGTv();
+//
+//        a샤오미Tv.켜기();
+//        // 출력 => 샤오미Tv 켜집니다.
+//        a샤오미Tv.끄기();
+//        // 출력 => 샤오미Tv 꺼집니다.
+//        a샤오미Tv.vr켜기();
+//        // 출력 => 샤오미Tv vr켜기!
+//
+//        a삼성Tv.켜기();
+//        // 출력 => 삼성Tv 켜집니다.
+//        a삼성Tv.끄기();
+//        // 출력 => 삼성Tv 꺼집니다.
+//        a삼성Tv.ar켜기();
+//        // 출력 => 삼성Tv ar켜기!
+//
+//        aLGTv.켜기();
+//        // 출력 => LGTv 켜집니다.
+//        aLGTv.끄기();
+//        // 출력 => LGTv 꺼집니다.
+//        aLGTv.게임모드전환();
+//        // 출력 => LGTv 게임모드전환!
+//    }
+//}
+//class 샤오미Tv{
+//    static void 켜기(){
+//        System.out.println("샤오미Tv 켜집니다.");
+//    }
+//    static void 끄기(){
+//        System.out.println("샤오미Tv 꺼집니다..");
+//    }
+//    static void vr켜기(){
+//        System.out.println("샤오미Tv vr켜기!");
+//    }
+//}
+//class 삼성Tv{
+//    static void 켜기(){
+//        System.out.println("삼성Tv 켜집니다.");
+//    }
+//    static void 끄기(){
+//        System.out.println("삼성Tv 꺼집니다..");
+//    }
+//    static void ar켜기(){
+//        System.out.println("삼성Tv ar켜기!");
+//    }
+//}
+//class LGTv {
+//    static void 켜기(){
+//        System.out.println("LGTv 켜집니다.");
+//    }
+//    static void 끄기(){
+//        System.out.println("샤오미Tv 꺼집니다..");
+//    }
+//    static void 게임모드전환(){
+//        System.out.println("LGTv 게임모드전환!");
+//    }
+//
+//}
