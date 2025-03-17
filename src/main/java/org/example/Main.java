57774123319cdcfd6a18e7366b4a8391bdedc153
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-17 14:04:54 +0900

    백준 8번 문제 해설

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,18 @@
 package org.example;
-// 문제 : 아래가 실행되도록 해주세요.
-
+//(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+//(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+//세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
 import java.util.Scanner;
-//7번 문제
-//준하는 사이트에 회원가입을 하다가 joonas라는 아이디가 이미 존재하는 것을 보고 놀랐다. 준하는 놀람을 ??!로 표현한다.
-// 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어졌을 때,
-// 놀람을 표현하는 프로그램을 작성하시오.
+
 class Main {
     public static void main(String[] args) {
-        String id = "joonas";//문자열 변수 id에 값 "joonas"를 저장 정확히는 변수명은 스택에 저장되고 값은 힙에 저장됨 이미 등록된 아이디 역활
-        Scanner sc = new Scanner(System.in);//스캐너 sc에 Scanner이라는 객체의 리모컨을 넘김 스캐너 사용할수 있게하는 역활
-        String newid = sc.next();//문자열 변수 newid에 sc.next()라는 메서드 리턴 값을 전달 새로 가입할 아이디 적는 역활
-        if (id.equals(newid)) {//만약 newid와enwid가 같은지 검사
-            System.out.println(newid + "??!");//위의 검사결과가 참이면 문자열 newid에 ??!를 추가해서 출력
-        }
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();//정수형 변수 a 에 스캐너의 nextInt() 매서드 리턴값을 저장 당연히 int형으로 나옴
+        int b = sc.nextInt();//정수형 변수 b 에 스캐너의 nextInt() 매서드 리턴값을 저장 당연히 int형으로 나옴
+        int c = sc.nextInt();//정수형 변수 c 에 스캐너의 nextInt() 매서드 리턴값을 저장 당연히 int형으로 나옴
+        System.out.println((a+b)%c); // a에 있는 값과 b에 있는 값을 더하고 c에있는 값으로 나눴을때의 나머지를 출력하라
+        System.out.println(((a%c)+(b%c))%c);//a값을 c값으로 나눴을때나머지와 b값을c값으로 나눴을때의 나머지를 먼저 더하고 그 값을 c값으로 다시 나눠을때의 나머지를 출력하라
+        System.out.println((a*b)%c);// 첫번째 출력문과 다른점은 사칙연산만 다름
+        System.out.println(((a%c)*(b%c))%c);// 위와 동일
     }
 }
-//
-//우선 입력받는 값을 사용하기위해 Scanner sc에 Scanner라는 클래스명을 가지고 매개변수를 하나가지는 Scanner을 new로 선언
-// 문자열 모양의 변수 newid에 스캐너클래스 안에있는 next()라는 메서드의 리턴값을 저장
-// 조건문 안의 조건 기존에 만들어 놓은 id "joonas"와 새로 입력 받는 아이디 값을 비교하기위한 조건 왜 ==을 쓰지않는가?
-// 우리는 문자열 변수 id 가 가지는 문자열 과 새로 입력받을 문자열 newid가 가지는 문자열 값 을 비교해야하는데 ==를 쓰면 주소값을 비교함
-// 그래서 equals를 사용해 그 주소 안에 있는 값을 꺼내서 비교함(참조형변수 또한 객체라고 볼수있음)
-// 조건식 안의 조건이 참이면 newid변수 안의 값에 문자열 ??!를 이어붙여서 출력함 거짓이면 그냥 끝남 왜냐면 거짓일때 해야할 행동을 만들어주지 않아서
