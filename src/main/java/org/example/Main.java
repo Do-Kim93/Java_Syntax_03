afabce0dd68df0041a87db4aa43c6363705ce9d2
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-17 11:39:27 +0900

    Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,31 @@
 package org.example;
 // 문제 : 아래가 실행되도록 해주세요.
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        // 철수나이 20
-        // 영희나이 22
-        // 영수나이 33 로 저장하기 여러가지 표현방법
-        //맵은 넣을땐 불편하지만 가져올 땐 편함
-        Map<String, Integer> agesMap = new HashMap<>();
-        agesMap.put("철수 나이", 20); //index : 철수(key)
-        agesMap.put("영희 나이", 22);
-        agesMap.put("영수 나이", 33);
-        System.out.println("map철수 나이 : " + agesMap.get("철수 나이"));
-        System.out.println("map영희 나이 : " + agesMap.get("영희 나이"));
-        System.out.println("map영수 나이 : " + agesMap.get("영수 나이"));
+        Scanner sc = new Scanner(System.in);
 
-        //ArrayList<>()는 공백을 허용하지 않는다. 즉 remove를 하면 한자리씩 밀려와서 빈자리를 채운다.
-        //ArrayList<>()는 넣을땐 편한데 가져올땐 불편함
-        List<Integer> ages = new ArrayList<>();
-        ages.add(20); // index : 0
-        ages.add(22);
-        ages.add(33);
+        //대기,숫자를 입력할 때까지
+        int a = 0;
 
-        System.out.println("철수 나이 : " + ages.get(0));
-        System.out.println("영희 나이 : " + ages.get(1));
-        System.out.println("영수 나이 : " + ages.get(2));
+        while (true){
+            System.out.println("숫자 : ");
+            try {
+                a = sc.nextInt();//nextInt()정수형만 입력가능
+                sc.nextLine();//이거 없으면 잘못된 값을 넣었을때 문한 루프에 빠진다.
+                break;
+            }catch (InputMismatchException e){
+                sc.nextLine();//저장 해놓은 버퍼(어떠한 값이 전달되면서 사용한 공간)를 비운다는 뜻이다
+                System.out.println("숫자 똑바로 써");
+            }
+        }
 
+
+        System.out.printf("입력된 숫자 %d\n", a);
+
+        sc.close();
     }
 }
 
