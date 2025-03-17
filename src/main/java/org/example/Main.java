95656dc4f6ba2b34c64c4c2080943c9ca6a79736
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-17 15:22:55 +0900

    백준 14번 문제 해설

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
+//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
 class Main {
     public static void main(String[] args) {
-        System.out.println("\\    /\\ \n )  ( ') \n(  /  ) \n \\(__)|");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a<b){//b가 a보다 크냐?? 조건식
+            System.out.println("<");//< 출력
+        } else if (a > b) {//a가 b보다 크냐?? 조건식
+            System.out.println(">");//> 출력
+        } else {//위의 조건식에 안들어가면 다여기로와
+            System.out.println("==");//==출력
+        }
     }
 }
-//출력할때 \역슬래쉬나 ',"등을 출력하고 싶으면 \뒤에적어주면 출력이 가능하다
+//비교연산자를 이용한 조건식 a가 b보다 작으면 <를 출력하고
+//a가 b보다 크면 >를 둘다 아니다 그러면 ==를 출력하게 한다.
